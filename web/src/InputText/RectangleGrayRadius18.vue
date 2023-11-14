2225bf1c4168aa2554a63cc12e54a6ec847b02cb
mitsuyan <dabido.mitsu@icloud.com>
2023-11-14 21:13:59 +0900

    update

---
+++ web/src/InputText/RectangleGrayRadius18.vue
@@ -0,0 +1,14 @@
+<template>
+    <input type="text" class="GrayRadius18" :text="text">
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            default: "Text"
+        }
+    }
+};
+</script>
