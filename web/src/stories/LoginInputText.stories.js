08f1b72c25188a5d2c8defd6880f524f48a0ccb2
mitsuyan <dabido.mitsu@icloud.com>
2023-11-15 01:04:55 +0900

    createstories

--- web/src/stories/LoginInputText.stories.js
+++ web/src/stories/LoginInputText.stories.js
@@ -1,13 +1,19 @@
 // LoginText.stories.js
-
 import LoginText from '../ComponentText/LoginText.vue';
+import RectangleGrayRadius18 from '../ComponentText/RectangleGrayRadius18.vue';
 //コンポーネントの設定
 export default {
     title: 'InputTextVariation',
     component: LoginText,
+    component: RectangleGrayRadius18
 };
 //ストーリーの追加
 export const LoginInputText = () => ({
     components: { LoginText },
     template: '<LoginText />'
+});
+
+export const rectangleGrayRadius18 = () => ({
+    components: { RectangleGrayRadius18 },
+    template: '<RectangleGrayRadius18 />'
 });
