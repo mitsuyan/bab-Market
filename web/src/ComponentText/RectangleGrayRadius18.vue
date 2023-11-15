793e870f3ceb641740c19d66bcd07df0f15a6212
mitsuyan <dabido.mitsu@icloud.com>
2023-11-15 10:03:34 +0900

    update file

--- web/src/ComponentText/RectangleGrayRadius18.vue
+++ web/src/ComponentText/RectangleGrayRadius18.vue
@@ -1,18 +1,29 @@
 <template>
-    <input type="text" class="grayRadius18" :text="text">
+    <input type="text" class="grayRadius18" v-model="text">
 </template>
 
 <script>
 export default {
     props: {
-        text: {
-            type: String,
-            default: "Text"
+        data: {
+            type: Object,
+            default: () => ({ text: "Text" })
         }
+    },
+    data() {
+        return {
+            text: this.data.text
+        };
     }
 };
 </script>
 
 <style>
-
+.grayRadius18 {
+    background-color: #727C8E;
+    border: none;
+    border-radius: 18pt;
+    width: 243pt;
+    height: 25pt;
+}
 </style>
