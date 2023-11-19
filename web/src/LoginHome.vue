61a779b1f11a534d5be189f9989614cb3100ef6e
mitsuyan <dabido.mitsu@icloud.com>
2023-11-19 16:23:40 +0900

    creat api

--- web/src/LoginHome.vue
+++ web/src/LoginHome.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="buttoIcon">
             <!-- buttonWidth プロパティに新しい値を設定 -->
-            <ButtonRedSemicircleIconLongShadow :buttonWidth="buttonWidth" :data="text" />
+            <ButtonRedSemicircleIconLongShadow :buttonWidth="buttonWidth" :data="text" @click="handleLogin" />
         </div>
         <div class="createLink">
             <accountLink />
@@ -22,6 +22,7 @@ import LoginText from './ComponentText/LoginText.vue';
 import ButtonRedSemicircleIconLongShadow from './ComponentButton/ButtonRedSemicircleIconLongShadow.vue';
 import headerIcon from './Components/headerIcon.vue';
 import accountLink from './Components/accountLink.vue';
+import apiServices from '../services/apiService.js';
 export default {
     name: 'LoginMain',
     components: {
@@ -34,9 +35,24 @@ export default {
         return {
             // ボタンの初期幅を変更
             buttonWidth: '220px',
-            text: 'ログイン'
+            text: 'ログイン',
+            userMail: 'mail',
+            userPass: 'pass'
         };
     },
+    methods: {
+        async handleLogin() {
+            try{
+                const response = apiServices.login({
+                    userMail: this.userMail,
+                    userPass: this.userPass
+                });
+                console.log(response);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
 }
 </script>
 
