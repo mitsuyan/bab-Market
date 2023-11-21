afc2c58539f6fdeea8e62303f5eb8dbf2f3d29c6
mitsuyan <dabido.mitsu@icloud.com>
2023-11-22 01:28:26 +0900

    createnavi

---
+++ web/src/stories/fototerNavigation.stories.js
@@ -0,0 +1,10 @@
+import footerNavigation from '../Component/navigation/footerNavigation.vue';
+
+export default {
+    title: 'Navigation'
+}
+
+export const footernavigation = () => ({
+    components: { footerNavigation },
+    template: '<footerNavigation />'
+});
