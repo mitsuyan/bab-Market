555d9583dc68320e8323d51b501b04571db46ea6
mitsuyan <dabido.mitsu@icloud.com>
2023-12-04 14:26:06 +0900

    up

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    proxy: {
+      '/app/api': {
+        target: 'https://aso-2201402.main.jp',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/app/api': '/app/api'
+        }
+      }
+    }
+  }
+}
